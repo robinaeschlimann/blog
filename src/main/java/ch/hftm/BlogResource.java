b7f75e731c250fbb25b9481fcaf555139c37bf76
robin.aeschlimann <robin.aeschlimann@hftm.ch>
2023-04-30 14:28:27 +0200

    Add BlogResource to get a message after calling /blog

--- src/main/java/ch/hftm/BlogResource.java
+++ src/main/java/ch/hftm/BlogResource.java
@@ -1,2 +1,16 @@
-package ch.hftm;public class BlogResource {
+package ch.hftm;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+@Path( "blog" )
+public class BlogResource
+{
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello() {
+        return "Willkommen auf meinem Blog!";
+    }
 }
