7236cd6981082fdd9685888ff0c7144eb5e15379
robin.aeschlimann <robin.aeschlimann@hftm.ch>
2023-06-23 09:36:42 +0200

    feat: new endpoints to add and get blogs

--- src/main/java/ch/hftm/BlogResource.java
+++ src/main/java/ch/hftm/BlogResource.java
@@ -1,16 +1,31 @@
 package ch.hftm;
 
+import ch.hftm.model.Blog;
+import ch.hftm.service.BlogService;
+import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
-@Path( "blog" )
+import java.util.List;
+
+@Path( "blogs" )
 public class BlogResource
 {
+    @Inject
+    BlogService service;
+
     @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "Willkommen auf meinem Blog!";
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Blog> getBlogs() {
+        return this.service.getBlogs();
+    }
+
+    @POST
+    public void addBlog( Blog blog )
+    {
+        this.service.addBlog( blog );
     }
 }
