d841250c271c90807612d5a1c4ef5d4d7ba0871a
robin.aeschlimann <robin.aeschlimann@hftm.ch>
2023-08-31 20:13:13 +0200

    feat: test for invalid token

--- src/test/java/ch/hftm/BlogResourceTest.java
+++ src/test/java/ch/hftm/BlogResourceTest.java
@@ -1,9 +1,22 @@
 package ch.hftm;
 
 import io.quarkus.test.junit.QuarkusTest;
+import io.quarkus.test.keycloak.client.KeycloakTestClient;
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.Test;
+
+import static io.quarkus.test.keycloak.server.KeycloakTestResourceLifecycleManager.getAccessToken;
 
 @QuarkusTest
 public class BlogResourceTest
 {
+    KeycloakTestClient keycloakCient = new KeycloakTestClient();
 
+    @Test
+    public void testInvalidToken() {
+        RestAssured.given().auth().oauth2("TOKEN").body("{ \"title\": \"Test\", \"content\": \"Test\" }")
+                .when().post( "/blog" )
+                .then()
+                .statusCode( 401 );
+    }
 }
